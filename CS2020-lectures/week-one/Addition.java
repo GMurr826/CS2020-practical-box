93baf75cf675040a03e0e6f49365a2204f809937
GRAHAM MURRAY <g.murray1.22@abdn.ac.uk>
2023-09-22 13:42:27 +0000

    addition example

--- CS2020-lectures/week-one/Addition.java
+++ CS2020-lectures/week-one/Addition.java
@@ -3,6 +3,10 @@ public class Addition{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String name = input.next();
+        System.out.printf("Hello %s%n", name);
+
         System.out.print("Enter the first integer: ");
         int number1 = input.nextInt();
 
